543ff87046367bdbe809cf2f0d398e14714ccd16
Jonas Rudlang <jonas.rudlang@gmail.com>
2016-12-04 00:17:33 +0100

    Add graphiql

--- graphql.go
+++ graphql.go
@@ -19,6 +19,9 @@ func main() {
 	// create graphql endpoint
 	http.Handle("/graphql", h)
 
+	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write(page)
+	}))
 	// serve!
 	port := ":8080"
 	log.Printf(`GraphQL server starting up on http://localhost%v`, port)
@@ -27,3 +30,41 @@ func main() {
 		log.Fatalf("ListenAndServe failed, %v", err)
 	}
 }
+
+var page = []byte(`
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/graphiql/0.7.8/graphiql.css" />
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/fetch/1.0.0/fetch.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.3.2/react.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.3.2/react-dom.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/graphiql/0.7.8/graphiql.js"></script>
+	</head>
+	<body style="width: 100%; height: 100%; margin: 0; overflow: hidden;">
+		<div id="graphiql" style="height: 100vh;">Loading...</div>
+		<script>
+			function graphQLFetcher(graphQLParams) {
+				graphQLParams.variables = graphQLParams.variables ? JSON.parse(graphQLParams.variables) : null;
+				return fetch("/graphql", {
+					method: "post",
+					body: JSON.stringify(graphQLParams),
+					credentials: "include",
+				}).then(function (response) {
+					return response.text();
+				}).then(function (responseBody) {
+					try {
+						return JSON.parse(responseBody);
+					} catch (error) {
+						return responseBody;
+					}
+				});
+			}
+			ReactDOM.render(
+				React.createElement(GraphiQL, {fetcher: graphQLFetcher}),
+				document.getElementById("graphiql")
+			);
+		</script>
+	</body>
+</html>
+`)
